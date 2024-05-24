72a42963054b230eca3effda267b512ea849f179
MaxAleBe <beck.maxime@online.de>
2024-05-24 10:38:53 +0200

    api

--- src/App.svelte
+++ src/App.svelte
@@ -33,7 +33,7 @@
   }
   
   async function showStars() {
-    const response = await axios.get('http://localhost:3000/star-data');
+    const response = await axios.get('https://newstar-api.maximebeck.de/star-data');
     const stars = response.data;
 
     // Clear existing stars from the scene
